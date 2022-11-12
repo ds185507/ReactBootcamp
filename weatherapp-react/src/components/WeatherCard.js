2cfca4e13224460c644908f4c517f23efc854d5a
ds185507 <114146583+ds185507@users.noreply.github.com>
2022-11-12 16:40:03 +0530

    UseEffect Hook added

--- weatherapp-react/src/components/WeatherCard.js
+++ weatherapp-react/src/components/WeatherCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../index.css";
 import WeatherSearchComp from "./WeatherSearchComp";
 import WeatherInfoComp from "./WeatherInfoComp";
@@ -7,40 +7,32 @@ import config from "../config";
 const API_KEY = config.WEATHER_KEY;
 
 function WeatherCard() {
-  function getCoordintes() {
-    function getCurrLocName(lat, lon, API_KEY) {
-      fetch(
-        `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${API_KEY}`
-      )
-        .then((res) => {
-          if (!res.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
-          }
-          return res.json();
-        })
-        .then((data) => getWeather(data[0].name, API_KEY));
-    }
-
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
+  useEffect(() => {
     function success(pos) {
       var crd = pos.coords;
       var lat = crd.latitude.toString();
       var lng = crd.longitude.toString();
+      function getCurrLocName(lat, lon, API_KEY) {
+        fetch(
+          `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${API_KEY}`
+        )
+          .then((res) => {
+            if (!res.ok) {
+              alert("No weather found.");
+              throw new Error("No weather found.");
+            }
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data[0].name);
+            getWeather(data[0].name, API_KEY);
+          });
+      }
+
       getCurrLocName(lat, lng, API_KEY);
     }
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  }
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
 
   function getWeather(name) {
     fetch(
@@ -48,7 +40,7 @@ function WeatherCard() {
     )
       .then((res) => {
         if (!res.ok) {
-          setFlag(2)
+          setFlag(2);
         }
         return res.json();
       })
@@ -79,7 +71,7 @@ function WeatherCard() {
   //2 -> invalid input
   const [data, setData] = useState(null);
 
-  if (flag===0) getCoordintes();
+  // if (flag===0) getCoordintes();
 
   return (
     <div className="card">
